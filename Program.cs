6328c5980ad0d4d854a152cb075c910cf4dfae66
basakbilginoglu <basakbilginoglu@gmail.com>
2021-02-07 02:12:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Mesut Özil");
+            myDictionary.Add(2, "Ayşe Geçer");
+            myDictionary.Add(3, "Aslı Tandoğan");
+
+            myDictionary.ToList();
+            Console.WriteLine(myDictionary.Count()); 
+        }
+    }
+}
